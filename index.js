ee67e4a3b5c5448bdc317d3f12b5bfb9df569a7a
ZahidHasanE <jahid1214hasan@gmail.com>
2025-03-07 23:30:52 +0600

    complete

--- index.js
+++ index.js
@@ -8,16 +8,65 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
+//connect database
+const username=process.env.MONGODB_USERNAME;
+const password=process.env.MONGODB_PASSWORD;
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.xlwhe.mongodb.net/registrationFormDB?retryWrites=true&w=majority`);
+
+
+//registration schema
+const registrationSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    password : String,
+});
+
+//model of registration Schema
+const Registration = mongoose.model("Registration",registrationSchema)
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/pages/index.html");
 });
 
 
-app.post("/register", (req,res) => {
+app.post("/register", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
 
-})
+        //To check duplicate emails
+        const exitsingUser = await Registration.findOne({email: email});
+        if(!exitsingUser){
+            const registrationData = new Registration({
+                name,
+                email,
+                password,
+            });
+            await registrationData.save();
+            res.redirect("/success");
+        }
+        else{
+            console.log("User already Exist");
+            res.redirect("/error");
+        }
 
+        
+
+    } catch (error) {
+        console.log(error);
+        res.redirect("/error");
+    }
+});
+
+app.get("/success", (req,res) => {
+    res.sendFile(__dirname + "/pages/success.html");
+});
+
+app.get("/error",(req,res) =>{
+    res.sendFile(__dirname + "/pages/error.html");
+});
 
 
 
